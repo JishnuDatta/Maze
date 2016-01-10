4d23613a0ad9866a4e9a32d76735ea968dc158f3
Jishnu Datta <jishnudattany@gmail.com>
2016-01-10 12:23:55 +0800

    Commit 5:
    Done:
    -Ladders are fully fixed. Game is now ready for phase 2.
    
    Todo:
    -Begin phase 2 => Start turnings black boxes into box2d objects and scale the map.

--- core/src/me/jishnu/mazegame/MazeGenerator.java
+++ core/src/me/jishnu/mazegame/MazeGenerator.java
@@ -1,6 +1,5 @@
 package me.jishnu.mazegame;
 
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -25,9 +24,7 @@ public class MazeGenerator {
         finished = false;
         setUpMaze();
 //        while(!finished){
-//            generateMazeStep();
-//        }
-        //printMaze();
+
     }
 
     public void setUpMaze() {
@@ -53,11 +50,11 @@ public class MazeGenerator {
     }
 
     public void update(float dt){
-        if(timer > 0.01f){
-            generateMazeStep();
-            timer = 0;
-        }
-        timer += dt;
+            //if (timer > 0.01f) {
+                generateMazeStep();
+                //timer = 0;
+           // }
+            timer += dt;
     }
 
     public void generateMazeStep(){
@@ -115,7 +112,6 @@ public class MazeGenerator {
 
     private void createLadders(int f){
         Array<Coordinates> potentialLadders = new Array<Coordinates>();
-        //Down ladders
         for(int x = 0; x < maxDimension; x+=2){
             for(int y = 0; y < maxDimension; y+=2){
                 if(mazeArray[f][x][y] == 1 && zDirectionHelper1(new Coordinates(f, x, y))) {
@@ -125,15 +121,22 @@ public class MazeGenerator {
         }
         if(potentialLadders.size > 0) {
             int ladders = ThreadLocalRandom.current().nextInt(1, 2 + 2 * (floors - f));
+            if(potentialLadders.size < ladders) {
+                ladders = potentialLadders.size;
+            }
             for(int i = 0; i < ladders; i++){
                 int index = ThreadLocalRandom.current().nextInt(0, potentialLadders.size);
-                mazeArray[f][potentialLadders.get(index).x][potentialLadders.get(index).y] = 6;
-                mazeArray[f - 1][potentialLadders.get(index).x][potentialLadders.get(index).y] = 9;
-                zDirectionHelper2(new Coordinates(f - 1, potentialLadders.get(index).x, potentialLadders.get(index).y));
+                if(zDirectionHelper2(new Coordinates(f - 1, potentialLadders.get(index).x, potentialLadders.get(index).y))){
+                    mazeArray[f][potentialLadders.get(index).x][potentialLadders.get(index).y] = 6;
+                    mazeArray[f - 1][potentialLadders.get(index).x][potentialLadders.get(index).y] = 9;
+                    potentialLadders.removeIndex(index);
+                }
+                else{
+                    potentialLadders.removeIndex(index);
+                }
             }
         }
     }
-
 
     //Returns true only 1 path around the current coordinate
     private boolean zDirectionHelper1(Coordinates c){
@@ -150,38 +153,45 @@ public class MazeGenerator {
         if(mazeArray[c.f][c.x][c.y-1] == 1){
             howMany ++;
         }
-        if(howMany == 1){
-            return true;
-        }
-            return false;
+            return (howMany == 1);
     }
 
-    private void zDirectionHelper2(Coordinates c){
+    private boolean zDirectionHelper2(Coordinates c){
         Array<Coordinates> possibleExits = new Array<Coordinates>();
+        Array<Coordinates> possibleNewCell = new Array<Coordinates>();
+        if(mazeArray[c.f][c.x][c.y] != 1){
         if(mazeArray[c.f][c.x - 1][c.y] == 7 && mazeArray[c.f][c.x - 2][c.y] == 7){
             possibleExits.add(new Coordinates(c.f, c.x - 1, c.y));
+            possibleNewCell.add(new Coordinates(c.f, c.x - 2, c.y));
         }
         //Right
         if(mazeArray[c.f][c.x + 1][c.y] == 7 && mazeArray[c.f][c.x + 2][c.y] == 7){
             possibleExits.add(new Coordinates(c.f, c.x + 1, c.y));
+            possibleNewCell.add(new Coordinates(c.f, c.x + 2, c.y));
         }
         //Backwards
         if(mazeArray[c.f][c.x][c.y - 1] == 7 && mazeArray[c.f][c.x][c.y - 2] == 7){
             possibleExits.add(new Coordinates(c.f, c.x, c.y - 1));
+            possibleNewCell.add(new Coordinates(c.f, c.x, c.y - 2));
         }
         //Forwards
-        if(mazeArray[c.f][c.x][c.y + 1] == 7 && mazeArray[c.f][c.x - 2][c.y + 2] == 7){
+        if(mazeArray[c.f][c.x][c.y + 1] == 7 && mazeArray[c.f][c.x][c.y + 2] == 7){
             possibleExits.add(new Coordinates(c.f, c.x, c.y + 1));
+            possibleNewCell.add(new Coordinates(c.f, c.x, c.y + 2));
         }
         if(possibleExits.size > 0) {
             int index = ThreadLocalRandom.current().nextInt(0, possibleExits.size);
             Coordinates possibleExit = possibleExits.removeIndex(index);
-            wallList.add(possibleExit);
-            //mazeArray[possibleExit.f][possibleExit.x][possibleExit.y] = 1;
+            Coordinates possibleCell = possibleNewCell.removeIndex(index);
+            mazeArray[possibleExit.f][possibleExit.x][possibleExit.y] = 1;
+            mazeArray[possibleCell.f][possibleCell.x][possibleCell.y] = 1;
+            addToCoordinatesList(possibleCell);
             for (Coordinates blocked : possibleExits) {
                 mazeArray[blocked.f][blocked.x][blocked.y] = 0;
             }
-        }
+            return true;
+        }}
+        return false;
     }
 
     private void addToCoordinatesList(Coordinates newCell){
@@ -200,20 +210,6 @@ public class MazeGenerator {
         //Forwards
         if(mazeArray[newCell.f][newCell.x][newCell.y + 1] == 7){
             wallList.add(new Coordinates(newCell.f, newCell.x, newCell.y + 1));
-        }
-    }
-
-    public void printMaze(){
-        for(int f = 0; f < floors; f++){
-            System.out.println("Floor: " + f);
-            System.out.println();
-            for (int y = maxDimension -1; y >= 0 ; y--){
-                for(int x = 0; x < maxDimension; x++){
-                    System.out.print(mazeArray[f][x][y]);
-                }
-                System.out.println();
-            }
-            System.out.println();
         }
     }
 
